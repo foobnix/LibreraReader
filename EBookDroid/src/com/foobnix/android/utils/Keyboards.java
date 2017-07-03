e1c18f5a5a4c1604f9391b71bf9dce20baee5ca0
Ivan Ivanenko <ivan.ivanenko@gmail.com>
2017-07-03 17:33:56 +0300

    improve fullscreen

--- EBookDroid/src/com/foobnix/android/utils/Keyboards.java
+++ EBookDroid/src/com/foobnix/android/utils/Keyboards.java
@@ -2,7 +2,6 @@ package com.foobnix.android.utils;
 
 import com.foobnix.pdf.info.wrapper.AppState;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Build;
@@ -29,54 +28,34 @@ public class Keyboards {
         inputManager.hideSoftInputFromWindow(currentFocus.getWindowToken(), 0);
     }
 
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-    public static void hideNavigation(Activity a) {
+    public static void hideNavigation(final Activity a) {
         try {
             if (!AppState.get().isFullScreen) {
                 return;
             }
-            if (Build.VERSION.SDK_INT >= 19) {
-                a.getWindow().getDecorView().setSystemUiVisibility(//
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE //
-                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION//
-                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN//
-                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION//
-                                | View.SYSTEM_UI_FLAG_FULLSCREEN//
-                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);//
-            }else if(Build.VERSION.SDK_INT >= 14){
-                a.getWindow().getDecorView().setSystemUiVisibility( //
-                        View.SYSTEM_UI_FLAG_LOW_PROFILE //
-                                | View.SYSTEM_UI_FLAG_FULLSCREEN); //
-            }
+            final View decorView = a.getWindow().getDecorView();
+            decorView.postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    if (Build.VERSION.SDK_INT >= 19) {
+                        decorView.setSystemUiVisibility(//
+                                View.SYSTEM_UI_FLAG_LAYOUT_STABLE //
+                                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION//
+                                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN//
+                                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION//
+                                        | View.SYSTEM_UI_FLAG_FULLSCREEN//
+                                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);//
+                    } else if (Build.VERSION.SDK_INT >= 14) {
+                        decorView.setSystemUiVisibility( //
+                                View.SYSTEM_UI_FLAG_LOW_PROFILE //
+                                        | View.SYSTEM_UI_FLAG_FULLSCREEN); //
+                    }
+                }
+
+            }, 100);
         } catch (Exception e) {
             LOG.e(e);
-        }
-
-    }
-
-    public static void hideHavigationBarOld(final Activity a) {
-        final View decorView = a.getWindow().getDecorView();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            decorView.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    decorView.setSystemUiVisibility(//
-                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE//
-                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION //
-                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION //
-                                    | View.SYSTEM_UI_FLAG_IMMERSIVE);
-                }
-            }, 100);
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-            decorView.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    decorView.setSystemUiVisibility( //
-                            View.SYSTEM_UI_FLAG_LOW_PROFILE //
-                                    | View.SYSTEM_UI_FLAG_FULLSCREEN //
-                    );
-                }
-            }, 100);
         }
 
     }
