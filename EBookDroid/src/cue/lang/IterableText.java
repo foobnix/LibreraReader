9385010939d1edfd03ea4d736f6aeb44aa9a80a7
Ivan Ivanenko <ivan.ivanenko@gmail.com>
2017-07-19 10:42:35 +0300

    remove not used

--- EBookDroid/src/cue/lang/IterableText.java
+++
@@ -1,9 +0,0 @@
-package cue.lang;
-
-import java.util.Iterator;
-
-abstract class IterableText implements Iterator<String>, Iterable<String> {
-	public Iterator<String> iterator() {
-		return this;
-	}
-}
