78047c41610cc197e4e1a2a32434f32af4c45b98
Ivan Ivanenko <ivan.ivanenko@gmail.com>
2017-11-13 17:15:41 +0200

    what is new

--- Builder/screenshot.sh
+++ Builder/screenshot.sh
@@ -7,5 +7,5 @@ adb shell screencap -p /sdcard/screen.png
 #adb shell am broadcast -a ScreenActivity --es locale "0,0,'$text'"
 
 
-adb pull /sdcard/screen.png $1/$2.png
+adb pull /sdcard/screen.png "$1/$2.png"
 adb shell rm /sdcard/screen.png
