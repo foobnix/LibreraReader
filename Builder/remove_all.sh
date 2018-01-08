76b541d2c5d829d135d06cedd0589a1436a9b592
Ivan Ivanenko <ivan.ivanenko@gmail.com>
2018-01-08 23:42:05 +0200

    beta .101

--- Builder/remove_all.sh
+++ Builder/remove_all.sh
@@ -1,3 +1,4 @@
+echo "uninstall"
 sh ninja-adb.sh uninstall classic.pdf.reader.viewer.djvu.epub.fb2.txt.mobi.book.reader.lirbi.libri
 sh ninja-adb.sh uninstall com.foobnix.pro.pdf.reader
 sh ninja-adb.sh uninstall com.foobnix.pdf.reader
@@ -5,7 +6,5 @@ sh ninja-adb.sh uninstall droid.reader.book.epub.mobi.pdf.djvu.fb2.txt.azw.azw3
 sh ninja-adb.sh uninstall com.example.zcontentintent
 sh ninja-adb.sh uninstall mobi.librera.book.reader
 sh ninja-adb.sh uninstall com.ebooka.pdf
-
-
 
 
