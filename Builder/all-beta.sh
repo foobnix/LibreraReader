808147a2bc5bb2a345b51c922d9ecaa4d9f2f055
Ivan Ivanenko <ivan.ivanenko@gmail.com>
2018-02-15 14:02:34 +0200

    build with mupdf 1.12 and mupdf 1.11

--- Builder/all-beta.sh
+++ Builder/all-beta.sh
@@ -2,19 +2,20 @@
 #PATH=/home/papa/dev/apache-ant-1.9.4/bin:$PATH
 #PATH=/home/papa/dev/android-sdk-linux/platform-tools:$PATH
 
-./link_to_mupdf_1.11.sh
 ./inc-index.sh
 
 ant clean-apk
 
 rm /home/ivan-dev/Dropbox/FREE_PDF_APK/testing/*.apk
 
-ant arm+arm64 pdf-beta
-#ant arm64 pdf-beta
-#ant x86 pdf-beta
+./link_to_mupdf_1.11.sh
+ant arm+arm64 pdf-beta-111
+
+./link_to_mupdf_1.12.sh
+ant arm+arm64 pdf-beta-112
+
 
 ant x86
-
 ant version
 
 ./remove_all.sh
