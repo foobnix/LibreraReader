00b3bcee199e34e55f90facdc6d96dc16ebc8c02
Ivan Ivanenko <ivan.ivanenko@gmail.com>
2018-03-21 14:57:25 +0200

    improve view

--- Builder/jni-1.11/~mupdf/css-apply.c
+++ Builder/jni-1.11/~mupdf/css-apply.c
@@ -885,7 +885,7 @@ number_from_value(fz_css_value *value, float initial, int initial_unit)
 		return make_number(initial, initial_unit);
 
 	if (value->type == CSS_PERCENT)
-		return make_number(fz_css_strtof(value->data, NULL), N_PERCENT);
+		return make_number(fz_css_strtof(value->data, NULL)/2.5, N_PERCENT);
 
 	if (value->type == CSS_NUMBER)
 		return make_number(fz_css_strtof(value->data, NULL), N_NUMBER);
@@ -1272,6 +1272,17 @@ fz_apply_css_style(fz_context *ctx, fz_html_font_set *set, fz_css_style *style, 
 	style->margin[1] = number_from_property(match, "margin-right", 0, N_LENGTH);
 	style->margin[2] = number_from_property(match, "margin-bottom", 0, N_LENGTH);
 	style->margin[3] = number_from_property(match, "margin-left", 0, N_LENGTH);
+
+	if(style->margin[0].unit==N_SCALE && style->margin[0].value>2)style->margin[0].value=2;
+	if(style->margin[1].unit==N_SCALE && style->margin[1].value>2)style->margin[1].value=2;
+	if(style->margin[2].unit==N_SCALE && style->margin[2].value>2)style->margin[2].value=2;
+	if(style->margin[3].unit==N_SCALE && style->margin[3].value>2)style->margin[3].value=2;
+
+
+	if(style->margin[0].unit==N_SCALE && style->margin[0].value<-2)style->margin[0].value=-2;
+	if(style->margin[1].unit==N_SCALE && style->margin[1].value<-2)style->margin[1].value=-2;
+	if(style->margin[2].unit==N_SCALE && style->margin[2].value<-2)style->margin[2].value=-2;
+	if(style->margin[3].unit==N_SCALE && style->margin[3].value<-2)style->margin[3].value=-2;
 
 	style->padding[0] = number_from_property(match, "padding-top", 0, N_LENGTH);
 	style->padding[1] = number_from_property(match, "padding-right", 0, N_LENGTH);
