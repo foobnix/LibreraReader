f95d20f15621e7c68eed99e5de678a2bd89006d1
foobnix <ivan.ivanenko@gmail.com>
2018-06-11 15:59:44 +0300

    beta and pro

--- Builder/all-beta.sh
+++ Builder/all-beta.sh
@@ -15,6 +15,10 @@ rm /home/ivan-dev/Dropbox/FREE_PDF_APK/testing/*.apk
 ./link_to_mupdf_1.11.sh
 #ant arm pdf-beta
 ant arm+arm64 pdf-beta
+ant arm+arm64 pro
+
+ant x86 pdf-beta
+ant x86 pro
 
 ant x86
 ant version
