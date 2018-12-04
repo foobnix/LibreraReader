d06a3527d47a24870694cb1a408335debabffd43
foobnix <ivan.ivanenko@gmail.com>
2018-12-04 18:04:11 +0200

    update icons, improve odt

--- EBookDroid/src/at/stefl/opendocument/java/translator/style/GeneralStyleElementTranslator.java
+++ EBookDroid/src/at/stefl/opendocument/java/translator/style/GeneralStyleElementTranslator.java
@@ -1,14 +1,39 @@
 package at.stefl.opendocument.java.translator.style;
 
+import com.foobnix.android.utils.LOG;
+
+import at.stefl.opendocument.java.css.StyleProperty;
 import at.stefl.opendocument.java.translator.style.property.BorderPropertyTranslator;
 import at.stefl.opendocument.java.translator.style.property.LineThroughPropertyTranslator;
 import at.stefl.opendocument.java.translator.style.property.MarginPropertyTranslator;
+import at.stefl.opendocument.java.translator.style.property.PropertyTranslator;
 import at.stefl.opendocument.java.translator.style.property.UnderlinePropertyTranslator;
 import at.stefl.opendocument.java.translator.style.property.VerticalAlignPropertyTranslator;
 
-public class GeneralStyleElementTranslator extends
-        DefaultStyleElementTranslator {
-    
+public class GeneralStyleElementTranslator extends DefaultStyleElementTranslator {
+
+    static class MyEm implements PropertyTranslator {
+
+        @Override
+        public StyleProperty translate(String name, String value) {
+            try {
+                if (value.contains("pt")) {
+                    LOG.d("StyleProperty before", "font-size", value);
+                    float f = Float.parseFloat(value.replace("pt", ""));
+                    float em = Math.min(Math.max(1, f / 12), 2);
+                    String emValue = em + "em";
+                    LOG.d("StyleProperty after", "font-size", emValue);
+                    return new StyleProperty("font-size", emValue);
+                }
+            } catch (Exception e) {
+                LOG.e(e);
+            }
+
+            return new StyleProperty("font-size", value);
+        }
+
+    }
+
     public GeneralStyleElementTranslator() {
         addPropertyTranslator("fo:text-align");
         addDirectionPropertyTranslator("fo:margin");
@@ -16,38 +41,34 @@ public class GeneralStyleElementTranslator extends
         addDirectionPropertyTranslator("fo:border");
         addPropertyTranslator("style:column-width", "width");
         addPropertyTranslator("style:row-height", "height");
-        addDirectionPropertyTranslator("fo:border",
-                new BorderPropertyTranslator());
+        addDirectionPropertyTranslator("fo:border", new BorderPropertyTranslator());
         addPropertyTranslator("fo:margin", new MarginPropertyTranslator());
-        
-        addPropertyTranslator("fo:font-size");
+
+        addPropertyTranslator("fo:font-size", new MyEm());
         addPropertyTranslator("style:font-name", "font-family");
         addPropertyTranslator("fo:font-weight");
         addPropertyTranslator("fo:font-style");
-        addPropertyTranslator("fo:font-size");
+        // addPropertyTranslator("fo:font-size");
         addPropertyTranslator("fo:text-shadow");
         addPropertyTranslator("fo:color");
         addPropertyTranslator("fo:background-color");
         addPropertyTranslator("style:vertical-align");
-        addPropertyTranslator("style:text-underline-style",
-                new UnderlinePropertyTranslator());
-        addPropertyTranslator("style:text-line-through-style",
-                new LineThroughPropertyTranslator());
-        addPropertyTranslator("style:text-position",
-                new VerticalAlignPropertyTranslator());
-        
+        addPropertyTranslator("style:text-underline-style", new UnderlinePropertyTranslator());
+        addPropertyTranslator("style:text-line-through-style", new LineThroughPropertyTranslator());
+        addPropertyTranslator("style:text-position", new VerticalAlignPropertyTranslator());
+
         addPropertyTranslator("style:width");
         addPropertyTranslator("style:height");
-        
+
         // odp
         // addPropertyTranslator("draw:fill-color", "background-color");
-        
+
         // svg translation
         addPropertyTranslator("draw:fill-color", "fill");
         addPropertyTranslator("svg:stroke-color", "stroke");
-        
+
         addPropertyTranslator("fo:page-width", "width");
         addPropertyTranslator("fo:page-height", "height");
     }
-    
+
 }
