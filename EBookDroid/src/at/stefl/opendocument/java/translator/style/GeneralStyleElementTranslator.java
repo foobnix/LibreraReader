544b9de6f12e99594f248ac1f1ae0354cec5566e
foobnix <ivan.ivanenko@gmail.com>
2018-12-07 12:16:06 +0200

    little improve odt

--- EBookDroid/src/at/stefl/opendocument/java/translator/style/GeneralStyleElementTranslator.java
+++ EBookDroid/src/at/stefl/opendocument/java/translator/style/GeneralStyleElementTranslator.java
@@ -3,12 +3,7 @@ package at.stefl.opendocument.java.translator.style;
 import com.foobnix.android.utils.LOG;
 
 import at.stefl.opendocument.java.css.StyleProperty;
-import at.stefl.opendocument.java.translator.style.property.BorderPropertyTranslator;
-import at.stefl.opendocument.java.translator.style.property.LineThroughPropertyTranslator;
-import at.stefl.opendocument.java.translator.style.property.MarginPropertyTranslator;
 import at.stefl.opendocument.java.translator.style.property.PropertyTranslator;
-import at.stefl.opendocument.java.translator.style.property.UnderlinePropertyTranslator;
-import at.stefl.opendocument.java.translator.style.property.VerticalAlignPropertyTranslator;
 
 public class GeneralStyleElementTranslator extends DefaultStyleElementTranslator {
 
@@ -20,10 +15,10 @@ public class GeneralStyleElementTranslator extends DefaultStyleElementTranslator
                 if (value.contains("pt")) {
                     LOG.d("StyleProperty before", "font-size", value);
                     float f = Float.parseFloat(value.replace("pt", ""));
-                    float em = Math.min(Math.max(1, f / 12), 1.4f);
+                    float em = Math.min(Math.max(1, f / 12), 1.2f);
                     String emValue = em + "em";
                     LOG.d("StyleProperty after", "font-size", emValue);
-                    return new StyleProperty("font-size", emValue);
+                    return new StyleProperty("font-size", "1em");
                 }
             } catch (Exception e) {
                 LOG.e(e);
@@ -35,40 +30,43 @@ public class GeneralStyleElementTranslator extends DefaultStyleElementTranslator
     }
 
     public GeneralStyleElementTranslator() {
-        addPropertyTranslator("fo:text-align");
-        addDirectionPropertyTranslator("fo:margin");
-        addDirectionPropertyTranslator("fo:padding");
-        addDirectionPropertyTranslator("fo:border");
-        addPropertyTranslator("style:column-width", "width");
-        addPropertyTranslator("style:row-height", "height");
-        addDirectionPropertyTranslator("fo:border", new BorderPropertyTranslator());
-        addPropertyTranslator("fo:margin", new MarginPropertyTranslator());
+        // addPropertyTranslator("fo:text-align");
+        // addDirectionPropertyTranslator("fo:margin");
+        // addDirectionPropertyTranslator("fo:padding");
+        // addDirectionPropertyTranslator("fo:border");
+        // addPropertyTranslator("style:column-width", "width");
+        // addPropertyTranslator("style:row-height", "height");
+        // addDirectionPropertyTranslator("fo:border", new BorderPropertyTranslator());
+        // addPropertyTranslator("fo:margin", new MarginPropertyTranslator());
 
-        addPropertyTranslator("fo:font-size", new MyEm());
+        // addPropertyTranslator("fo:font-size", new MyEm());
         addPropertyTranslator("style:font-name", "font-family");
         addPropertyTranslator("fo:font-weight");
         addPropertyTranslator("fo:font-style");
         // addPropertyTranslator("fo:font-size");
-        addPropertyTranslator("fo:text-shadow");
+        // addPropertyTranslator("fo:text-shadow");
         addPropertyTranslator("fo:color");
-        addPropertyTranslator("fo:background-color");
-        addPropertyTranslator("style:vertical-align");
-        addPropertyTranslator("style:text-underline-style", new UnderlinePropertyTranslator());
-        addPropertyTranslator("style:text-line-through-style", new LineThroughPropertyTranslator());
-        addPropertyTranslator("style:text-position", new VerticalAlignPropertyTranslator());
+        // addPropertyTranslator("fo:background-color");
+        // addPropertyTranslator("style:vertical-align");
+        // addPropertyTranslator("style:text-underline-style", new
+        // UnderlinePropertyTranslator());
+        // addPropertyTranslator("style:text-line-through-style", new
+        // LineThroughPropertyTranslator());
+        // addPropertyTranslator("style:text-position", new
+        // VerticalAlignPropertyTranslator());
 
-        addPropertyTranslator("style:width");
-        addPropertyTranslator("style:height");
+        // addPropertyTranslator("style:width");
+        // addPropertyTranslator("style:height");
 
         // odp
         // addPropertyTranslator("draw:fill-color", "background-color");
 
         // svg translation
-        addPropertyTranslator("draw:fill-color", "fill");
-        addPropertyTranslator("svg:stroke-color", "stroke");
+        // addPropertyTranslator("draw:fill-color", "fill");
+        // addPropertyTranslator("svg:stroke-color", "stroke");
 
-        addPropertyTranslator("fo:page-width", "width");
-        addPropertyTranslator("fo:page-height", "height");
+        // addPropertyTranslator("fo:page-width", "width");
+        // addPropertyTranslator("fo:page-height", "height");
     }
 
 }
