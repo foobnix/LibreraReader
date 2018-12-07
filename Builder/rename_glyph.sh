433d0048c3622d74d1a052be5f08220b44f1eb09
foobnix <ivan.ivanenko@gmail.com>
2018-12-07 15:09:38 +0200

    update icons

--- Builder/rename_glyph.sh
+++ Builder/rename_glyph.sh
@@ -1,5 +1,6 @@
 #https://www.maketecheasier.com/rename-files-in-linux/
 rename -f 's/\@3x//' *.png
+rename -f 's/-basic//' *.png
 rename -f 's/-/_/' *.png
 rename -f 's/-/_/' *.png
 rename -f 's/-/_/' *.png
