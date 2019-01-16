37be90f5a37ae1b3c34a60b6f8731322945cc5e0
foobnix <ivan.ivanenko@gmail.com>
2019-01-16 17:05:04 +0200

    prepare new build

--- Builder/all-beta.sh
+++ Builder/all-beta.sh
@@ -10,9 +10,10 @@ ant clean-apk
 rm /home/ivan-dev/Dropbox/FREE_PDF_APK/testing/*.apk
 
 
-ant arm+arm64 tts-reader
 
-#ant beta-cpu pdf-beta
+ant beta-cpu pdf-beta
+ant arm tts-reader
+ant arm64 tts-reader
 
 #ant arm+arm64 pdf
 #ant x86 pdf
