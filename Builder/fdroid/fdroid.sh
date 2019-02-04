3249f385ebf53631c2c5bc9c7c7cb28b4e8ff756
foobnix <ivan.ivanenko@gmail.com>
2019-02-04 17:28:24 +0200

    add fdroid

---
+++ Builder/fdroid/fdroid.sh
@@ -0,0 +1,30 @@
+#git clone https://github.com/foobnix/LibreraReader.git --branch 7.12.67
+git clone https://github.com/foobnix/LibreraReader.git
+git clone --recursive git://git.ghostscript.com/mupdf.git --branch 1.11
+
+mkdir release
+
+cd LibreraReader
+git fetch
+#git reset --hard origin/master
+git pull origin master
+cd ..
+
+cd mupdf
+make
+cd ..
+
+LibreraReader/Builder/link_to_mupdf_1.11.sh mupdf LibreraReader
+
+pwd
+
+cd LibreraReader
+./update_all.sh
+./update_fix_build.sh
+
+cd Builder
+./update_jars.sh
+
+
+ant clean-apk
+ant arm+arg64 pro-droid
