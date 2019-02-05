c593a41b9ab4dc7062d58f4cfc20d9392e5e1d47
foobnix <ivan.ivanenko@gmail.com>
2019-02-05 16:43:26 +0200

    update jars

--- Builder/update_jars.sh
+++ Builder/update_jars.sh
@@ -10,10 +10,10 @@ wget -nc http://central.maven.org/maven2/org/greenrobot/greendao-api/3.2.2/green
 wget -nc http://central.maven.org/maven2/org/jsoup/jsoup/1.11.3/jsoup-1.11.3.jar
 wget -nc http://central.maven.org/maven2/com/googlecode/juniversalchardet/juniversalchardet/1.0.3/juniversalchardet-1.0.3.jar
 wget -nc http://central.maven.org/maven2/com/squareup/okhttp3/okhttp/3.12.1/okhttp-3.12.1.jar
-wget -nc http://repo.spring.io/plugins-release/com/burgstaller/okhttp-digest/1.18/okhttp-digest-1.18.jar
+wget -nc https://jcenter.bintray.com/com/burgstaller/okhttp-digest/1.18/okhttp-digest-1.18.jar
 wget -nc http://central.maven.org/maven2/com/squareup/okio/okio/1.17.3/okio-1.17.3.jar
 wget -nc http://central.maven.org/maven2/com/github/joniles/rtfparserkit/1.13.0/rtfparserkit-1.13.0.jar
 
-wget -nc https://github.com/CloudRail/cloudrail-si-android-sdk/blob/master/cloudrail-si-release.aar?raw=true -O CloudRail.aar
-unzip -p CloudRail.aar classes.jar >cloudrail.jar
+wget -nc https://repo1.maven.org/maven2/com/cloudrail/cloudrail-si-android/2.22.4/cloudrail-si-android-2.22.4.aar -O CloudRail.aar
+unzip -p CloudRail.aar classes.jar >cloudrail-2.22.4.jar
 rm CloudRail.aar
