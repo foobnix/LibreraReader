5416e786b496a1e2067f5263a606df15bcbd5e07
foobnix <ivan.ivanenko@gmail.com>
2019-02-22 17:03:06 +0200

    udpate builder

--- Builder/all-beta.sh
+++ Builder/all-beta.sh
@@ -4,9 +4,11 @@
 
 cd ../
 
-./gradlew --no-daemon incVersion
-./gradlew --no-daemon clean assembleBetaRelease copyApks -Pbeta
-./gradlew --no-daemon clean assembleEbookaRelease copyApks -Pbeta
+./gradlew clean incVersion
+./gradlew assembleBetaRelease
+./gradlew assembleEbookaRelease
+./gradlew copyApks -Pbeta
+./gradlew -stop
 
 cd Builder
 ./remove_all.sh
