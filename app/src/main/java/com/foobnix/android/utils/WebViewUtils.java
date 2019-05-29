0ced56ad91be51d0013b80fd590cf1f4beb0252d
foobnix <ivan.ivanenko@gmail.com>
2019-05-29 13:10:53 +0300

    continue svg

--- app/src/main/java/com/foobnix/android/utils/WebViewUtils.java
+++ app/src/main/java/com/foobnix/android/utils/WebViewUtils.java
@@ -36,42 +36,59 @@ public class WebViewUtils {
         web.layout(0, 0, Dips.screenMinWH(), Dips.screenMinWH());
     }
 
-    public interface WebViewResponse {
-
-        void onResult(String id, Bitmap bitmap);
-
-    }
-
 
     public static void renterToZip(String name, String content, ZipOutputStream zos, Object lock) {
 
+        handler.post(() -> web.loadData("", "text/html", "utf-8"));
         handler.post(() -> web.loadData(content, "text/html", "utf-8"));
 
-        handler.postDelayed(() -> {
-            int wh = Dips.screenMinWH();
 
-            Bitmap bitmap = Bitmap.createBitmap(wh, Math.min(wh, web.getContentHeight() + Dips.DP_50), Bitmap.Config.ARGB_8888);
-            Canvas c = new Canvas(bitmap);
-            web.draw(c);
+        Runnable execute = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    int wh = Dips.screenMinWH();
+
+                    Bitmap bitmap = Bitmap.createBitmap(wh, Math.min(wh, web.getContentHeight() + Dips.DP_50), Bitmap.Config.ARGB_8888);
+                    Canvas c = new Canvas(bitmap);
+                    web.draw(c);
 
 
-            ByteArrayOutputStream os = new ByteArrayOutputStream();
+                    ByteArrayOutputStream os = new ByteArrayOutputStream();
 
-            Bitmap.CompressFormat format = Bitmap.CompressFormat.PNG;
-            bitmap.compress(format, 100, os);
-            bitmap.recycle();
-            try {
-                LOG.d("SVG: writeToZipNoClose", name);
-                Fb2Extractor.writeToZipNoClose(zos, name, new ByteArrayInputStream(os.toByteArray()));
+                    Bitmap.CompressFormat format = Bitmap.CompressFormat.PNG;
+                    bitmap.compress(format, 100, os);
+                    bitmap.recycle();
+                    try {
+                        LOG.d("SVG: writeToZipNoClose", name);
+                        Fb2Extractor.writeToZipNoClose(zos, name, new ByteArrayInputStream(os.toByteArray()));
 
-            } catch (IOException e) {
-                LOG.e(e);
+                    } catch (IOException e) {
+                        LOG.e(e);
+                    }
+                } finally {
+                    synchronized (lock) {
+                        lock.notify();
+                    }
+                }
+
             }
-            synchronized (lock) {
-                lock.notify();
-            }
+        };
 
-        }, 500);
+        Runnable check = new Runnable() {
+
+            @Override
+            public void run() {
+                if (web.getContentHeight() > 2) {
+                    handler.post(execute);
+                } else {
+                    handler.postDelayed(this, 100);
+                }
+            }
+        };
+
+        handler.postDelayed(check, 300);
+
 
     }
 
