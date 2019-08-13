505cf4d82680c34f170e12a85735e41fae9de794
foobnix <ivan.ivanenko@gmail.com>
2019-08-13 11:08:58 +0300

    skip save state

--- app/src/main/java/com/foobnix/pdf/info/view/MyProgressBar.java
+++ app/src/main/java/com/foobnix/pdf/info/view/MyProgressBar.java
@@ -2,7 +2,6 @@ package com.foobnix.pdf.info.view;
 
 import android.content.Context;
 import android.os.Build;
-import android.os.Parcelable;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.widget.ProgressBar;
@@ -14,42 +13,26 @@ public class MyProgressBar extends ProgressBar {
     public MyProgressBar(Context context) {
         super(context);
         LOG.d("MyProgressBar-1", getId());
+        setSaveEnabled(false);
     }
 
     public MyProgressBar(Context context, AttributeSet attrs) {
         super(context, attrs);
         LOG.d("MyProgressBar-2", getId());
+        setSaveEnabled(false);
     }
 
     public MyProgressBar(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         LOG.d("MyProgressBar-3", getId());
+        setSaveEnabled(false);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public MyProgressBar(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
         LOG.d("MyProgressBar-4", getId());
+        setSaveEnabled(false);
     }
 
-    @Override
-    public Parcelable onSaveInstanceState() {
-        try {
-            return super.onSaveInstanceState();
-        } catch (Exception e) {
-            LOG.e(e);
-        }
-        return null;
-    }
-
-    @Override
-    public void onRestoreInstanceState(Parcelable state) {
-        try {
-            super.onRestoreInstanceState(state);
-        } catch (RuntimeException e) {
-            LOG.e(e);
-        } catch (Exception e) {
-            LOG.e(e);
-        }
-    }
 }
