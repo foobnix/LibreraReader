dbd605a0bf3d411f1e75b1bb989af8decc50522f
foobnix <ivan.ivanenko@gmail.com>
2020-04-23 11:13:24 +0300

    update share text

--- app/src/main/java/com/foobnix/zipmanager/SendReceiveActivity.java
+++ app/src/main/java/com/foobnix/zipmanager/SendReceiveActivity.java
@@ -1,18 +1,20 @@
 package com.foobnix.zipmanager;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 
 import com.foobnix.OpenerActivity;
 import com.foobnix.android.utils.LOG;
 import com.foobnix.android.utils.TxtUtils;
 import com.foobnix.opds.OPDS;
+import com.foobnix.pdf.info.IMG;
 import com.foobnix.pdf.info.model.BookCSS;
+import com.foobnix.pdf.search.view.AsyncProgressTask;
+import com.foobnix.ui2.MyContextWrapper;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -22,17 +24,45 @@ import java.io.File;
 import java.io.FileOutputStream;
 
 public class SendReceiveActivity extends Activity {
-    Handler handler = new Handler(Looper.getMainLooper());
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        updateIntent();
+
+        new AsyncProgressTask<Object>() {
+            @Override
+            public Context getContext() {
+                return SendReceiveActivity.this;
+            }
+
+            @Override
+            protected Object doInBackground(Object... objects) {
+                try {
+                    updateIntent();
+                } catch (Exception e) {
+                    LOG.e(e);
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Object result) {
+                super.onPostExecute(result);
+                startShareIntent();
+            }
+        }.execute();
+
+    }
+
+    @Override
+    protected void attachBaseContext(Context context) {
+        super.attachBaseContext(MyContextWrapper.wrap(context));
     }
 
     private void startShareIntent() {
         try {
+            IMG.clearMemoryCache();
             getIntent().setAction(Intent.ACTION_VIEW);
+            getIntent().addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             getIntent().setData(getIntent().getData());
             getIntent().setClass(this, OpenerActivity.class);
             startActivity(getIntent());
@@ -43,7 +73,7 @@ public class SendReceiveActivity extends Activity {
         }
     }
 
-    private void updateIntent() {
+    private void updateIntent() throws Exception {
         Bundle extras = getIntent().getExtras();
         LOG.d("updateIntent()-", getIntent());
         LOG.d("updateIntent()-getExtras", getIntent().getExtras());
@@ -63,71 +93,58 @@ public class SendReceiveActivity extends Activity {
 
                 LOG.d("updateIntent uri getScheme", uri.getScheme());
 
-                Thread thread = new Thread() {
-                    @Override
-                    public void run() {
-                        try {
 
-                            final String httpResponse = OPDS.getHttpResponse((String) text, "", "");
+                final String httpResponse = OPDS.getHttpResponse((String) text, "", "");
 
-                            boolean isText = false;
-                            if (TxtUtils.isNotEmpty(httpResponse) && !httpResponse.contains("<html")) {
-                                isText = true;
-                            }
-                            LOG.d("updateIntent()-is text", isText);
-                            //Document document = Jsoup.connect((String) text).userAgent("Mozilla/5.0 (jsoup)").timeout(30000).get();
-                            if (isText) {
-                                String
-                                        title = TxtUtils.fixFileName(TxtUtils.substringSmart(httpResponse, 30));
-                                File file = new File(BookCSS.get().downlodsPath, title + ".txt");
-                                file.getParentFile().mkdirs();
-                                LOG.d("outerHtml-file", file);
-                                FileOutputStream fileOutputStream = new FileOutputStream(file);
-                                LOG.d("outerHtml-text", httpResponse);
-                                fileOutputStream.write(httpResponse.getBytes());
-                                fileOutputStream.flush();
-                                fileOutputStream.close();
-                                getIntent().setData(Uri.fromFile(file));
+                boolean isText = false;
+                if (TxtUtils.isNotEmpty(httpResponse) && !httpResponse.contains("<html")) {
+                    isText = true;
+                }
+                LOG.d("updateIntent()-is text", isText);
+                //Document document = Jsoup.connect((String) text).userAgent("Mozilla/5.0 (jsoup)").timeout(30000).get();
+                if (isText) {
+                    String
+                            title = TxtUtils.fixFileName(TxtUtils.substringSmart(httpResponse, 30));
+                    File file = new File(BookCSS.get().downlodsPath, title + ".txt");
+                    file.delete();
+                    file.getParentFile().mkdirs();
+                    LOG.d("outerHtml-file", file);
+                    FileOutputStream fileOutputStream = new FileOutputStream(file);
+                    LOG.d("outerHtml-text", httpResponse);
+                    fileOutputStream.write(httpResponse.getBytes());
+                    fileOutputStream.flush();
+                    fileOutputStream.close();
+                    getIntent().setData(Uri.fromFile(file));
 
-                                LOG.d("updateIntent save ready", file, file.getAbsolutePath());
-                            } else {
-                                Document document = Jsoup.parse(httpResponse);
+                    LOG.d("updateIntent save ready", file, file.getAbsolutePath());
+                } else {
+                    Document document = Jsoup.parse(httpResponse);
 
 
-                                String title = (document.title() + text).replaceAll("[^\\w]+", " ");
-                                if (title.length() > 31) {
-                                    title = TxtUtils.fixFileName(TxtUtils.substringSmart(title, 30));
-                                }
-
-                                File file = new File(BookCSS.get().downlodsPath, title + ".html");
-                                file.getParentFile().mkdirs();
-
-                                FileOutputStream fileOutputStream = new FileOutputStream(file);
-                                String outerHtml = Jsoup.clean(document.html(), Whitelist.basic());
-                                LOG.d("outerHtml-html", outerHtml);
-                                fileOutputStream.write("<html><head></head><body style='text-align:justify;'>".getBytes());
-                                fileOutputStream.write(outerHtml.getBytes());
-                                fileOutputStream.write("</body></html>".getBytes());
-                                fileOutputStream.flush();
-                                fileOutputStream.close();
-                                getIntent().setData(Uri.fromFile(file));
-
-                                LOG.d("updateIntent save ready", file, file.getAbsolutePath());
-
-                            }
-                        } catch (Throwable throwable) {
-                            LOG.e(throwable);
-                        }
-                        handler.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                startShareIntent();
-                            }
-                        });
-
+                    String title = (document.title() + text).replaceAll("[^\\w]+", " ");
+                    if (title.length() > 31) {
+                        title = TxtUtils.fixFileName(TxtUtils.substringSmart(title, 30));
                     }
-                };
-                thread.start();
+
+                    File file = new File(BookCSS.get().downlodsPath, title + ".html");
+                    file.delete();
+                    file.getParentFile().mkdirs();
+
+                    FileOutputStream fileOutputStream = new FileOutputStream(file);
+                    String outerHtml = Jsoup.clean(document.html(), Whitelist.basic());
+                    LOG.d("outerHtml-html", outerHtml);
+                    fileOutputStream.write("<html><head></head><body style='text-align:justify;'>".getBytes());
+                    fileOutputStream.write(outerHtml.getBytes());
+                    fileOutputStream.write("</body></html>".getBytes());
+                    fileOutputStream.flush();
+                    fileOutputStream.close();
+                    getIntent().setData(Uri.fromFile(file));
+
+                    LOG.d("updateIntent save ready", file, file.getAbsolutePath());
+
+                }
+
+
             } else {
 
                 File file = new File(BookCSS.get().downlodsPath, "temp.txt");
@@ -140,7 +157,6 @@ public class SendReceiveActivity extends Activity {
                     fileOutputStream.close();
                     getIntent().setData(Uri.fromFile(file));
                     LOG.d(" updateIntent setData temp.txt", file);
-                    startShareIntent();
                 } catch (Exception e) {
                     LOG.e(e);
                 }
