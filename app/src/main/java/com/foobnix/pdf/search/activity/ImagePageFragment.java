8ed7977ff812b8c82d64eee50dc70b4d8445d04a
Isira Seneviratne <isirasen96@gmail.com>
2020-07-20 12:50:30 +0530

    Use view binding in ImagePageFragment.

--- app/src/main/java/com/foobnix/pdf/search/activity/ImagePageFragment.java
+++ app/src/main/java/com/foobnix/pdf/search/activity/ImagePageFragment.java
@@ -8,14 +8,10 @@ import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.loader.app.LoaderManager;
-import androidx.loader.content.AsyncTaskLoader;
-import androidx.loader.content.Loader;
 
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.target.SimpleTarget;
@@ -23,161 +19,72 @@ import com.bumptech.glide.request.transition.Transition;
 import com.foobnix.android.utils.LOG;
 import com.foobnix.pdf.info.IMG;
 import com.foobnix.pdf.info.R;
+import com.foobnix.pdf.info.databinding.PageNBinding;
 import com.foobnix.pdf.info.wrapper.MagicHelper;
-import com.foobnix.sys.ImageExtractor;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 
 public class ImagePageFragment extends Fragment {
     public static final String POS = "pos";
     public static final String PAGE_PATH = "pagePath";
     public static final String IS_TEXTFORMAT = "isTEXT";
-    final static ExecutorService executorService = Executors.newSingleThreadExecutor();
     public static volatile int count = 0;
     int page;
     Handler handler;
     long lifeTime = 0;
-    int loadImageId;
-    boolean fistTime = true;
     SimpleTarget<Bitmap> target = null;
-    Future<?> submit;
-    private PageImaveView image;
-    private TextView text;
-    Runnable callback = new Runnable() {
-
-        @Override
-        public void run() {
-            if (!isDetached()) {
-                loadImageGlide();
-            } else {
-                LOG.d("Image page is detached");
-            }
+    private PageNBinding binding;
+    Runnable callback = () -> {
+        if (!isDetached()) {
+            loadImageGlide();
+        } else {
+            LOG.d("Image page is detached");
         }
-
     };
 
     public String getPath() {
+        assert getArguments() != null;
         return getArguments().getString(PAGE_PATH);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.page_n, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = PageNBinding.inflate(inflater, container, false);
 
+        assert getArguments() != null;
         page = getArguments().getInt(POS);
 
+        binding.myImage1.setPageNumber(page);
+        binding.text1.setText(getString(R.string.page) + " " + (page + 1));
 
-        text = (TextView) view.findViewById(R.id.text1);
-        image = (PageImaveView) view.findViewById(R.id.myImage1);
-
-        image.setPageNumber(page);
-        text.setText(getString(R.string.page) + " " + (page + 1));
-
-        text.setTextColor(MagicHelper.getTextColor());
+        binding.text1.setTextColor(MagicHelper.getTextColor());
 
         handler = new Handler();
-        handler.postDelayed(new Runnable() {
-
-            @Override
-            public void run() {
-                LOG.d("ImagePageFragment1  run ", page, "getPriority", getPriority(), "counter", count);
-                if (isVisible()) {
-                    if (0 == getPriority()) {
-                        handler.post(callback);
-                    } else if (1 == getPriority()) {
-                        handler.postDelayed(callback, 200);
-                    } else {
-                        handler.postDelayed(callback, getPriority() * 200);
-                    }
+        handler.postDelayed(() -> {
+            LOG.d("ImagePageFragment1  run ", page, "getPriority", getPriority(), "counter", count);
+            if (isVisible()) {
+                if (0 == getPriority()) {
+                    handler.post(callback);
+                } else if (1 == getPriority()) {
+                    handler.postDelayed(callback, 200);
+                } else {
+                    handler.postDelayed(callback, getPriority() * 200);
                 }
-
             }
         }, 150);
         lifeTime = System.currentTimeMillis();
 
-
-        return view;
-    }
-
-    public void loadImageGlide21() {
-
-        submit = executorService.submit(new Runnable() {
-            @Override
-            public void run() {
-                if (submit.isCancelled()) {
-                    LOG.d("loadImageGlide-isCancelled 1");
-                    return;
-                }
-                Bitmap bitmap = ImageExtractor.getInstance(getContext()).proccessOtherPage(getPath());
-
-                if (submit.isCancelled()) {
-                    LOG.d("loadImageGlide-isCancelled 2");
-                    return;
-                }
-
-                getActivity().runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        text.setVisibility(View.GONE);
-                        if (bitmap != null && image != null) {
-                            image.addBitmap(bitmap);
-                        }
-                    }
-                });
-
-            }
-        });
-
-    }
-
-    public void loadImageGlide2() {
-        final LoaderManager.LoaderCallbacks<Bitmap> callback = new LoaderManager.LoaderCallbacks<Bitmap>() {
-            @NonNull
-            @Override
-            public Loader<Bitmap> onCreateLoader(int id, @Nullable Bundle args) {
-                return new AsyncTaskLoader<Bitmap>(getContext()) {
-
-                    @Nullable
-                    @Override
-                    public Bitmap loadInBackground() {
-                        return ImageExtractor.getInstance(getContext()).proccessOtherPage(getPath());
-
-                    }
-                };
-            }
-
-            @Override
-            public void onLoadFinished(@NonNull Loader<Bitmap> loader, Bitmap data) {
-                LOG.d("loadImageGlide-onLoadFinished");
-
-                text.setVisibility(View.GONE);
-                if (data != null && image != null) {
-                    image.addBitmap(data);
-                }
-            }
-
-            @Override
-            public void onLoaderReset(@NonNull Loader<Bitmap> loader) {
-                LOG.d("loadImageGlide-onLoaderReset");
-            }
-        };
-        LoaderManager.getInstance(getActivity()).initLoader(getPath().hashCode(), null, callback).forceLoad();
+        return binding.getRoot();
     }
 
     public void loadImageGlide() {
-        if (image != null && image.getWidth() == 0) {
+        if (binding.myImage1.getWidth() == 0) {
             return;
         }
         target = new SimpleTarget<Bitmap>() {
             @Override
             public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
-                text.setVisibility(View.GONE);
-                if (resource != null && image != null) {
-                    image.addBitmap(resource);
-                }
+                binding.text1.setVisibility(View.GONE);
+                binding.myImage1.addBitmap(resource);
             }
         };
         LOG.d("Glide-load-into", getActivity());
@@ -191,19 +98,14 @@ public class ImagePageFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        if (image != null) {
-            image.clickUtils.init();
+        binding.myImage1.clickUtils.init();
 
-            float[] values = new float[10];
-            if (image != null) {
-                image.imageMatrix().getValues(values);
-                if (values[Matrix.MSCALE_X] == 0) {
-                    PageImageState.get().isAutoFit = true;
-                    image.autoFit();
-                    LOG.d("fonResume-autofit", page);
-                }
-            }
-
+        float[] values = new float[10];
+        binding.myImage1.imageMatrix().getValues(values);
+        if (values[Matrix.MSCALE_X] == 0) {
+            PageImageState.get().isAutoFit = true;
+            binding.myImage1.autoFit();
+            LOG.d("fonResume-autofit", page);
         }
     }
 
@@ -214,8 +116,8 @@ public class ImagePageFragment extends Fragment {
 
     @Override
     public void onDestroyView() {
-
         super.onDestroyView();
+        binding = null;
         LOG.d("loadImageGlide-onDestroyView");
 //        if (submit != null) {
 //            submit.cancel(true);
@@ -226,18 +128,15 @@ public class ImagePageFragment extends Fragment {
 //        }
 //        LoaderManager.getInstance(getActivity()).destroyLoader(getPath().hashCode());
 
-        if (Build.VERSION.SDK_INT >= 17 && !getActivity().isDestroyed()) {
+        if (Build.VERSION.SDK_INT >= 17 && !requireActivity().isDestroyed()) {
             IMG.clear(getActivity(), target);
-        } else if (!getActivity().isFinishing()) {
+        } else if (!requireActivity().isFinishing()) {
             IMG.clear(getActivity(), target);
         }
-
 
         LOG.d("ImagePageFragment1 onDetach ", page, "Lifi Time: ", System.currentTimeMillis() - lifeTime);
         if (handler != null) {
             handler.removeCallbacksAndMessages(null);
         }
-        image = null;
     }
-
 }
