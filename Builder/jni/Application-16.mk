a55238fb5ddb22e8c679e98f96a65c44a7a6f517
foobnix <ivan.ivanenko@gmail.com>
2022-08-31 15:48:05 +0300

    build Librera old for Android 4.0

---
+++ Builder/jni/Application-16.mk
@@ -0,0 +1,14 @@
+APP_PLATFORM=android-16
+
+APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
+#APP_ABI := x86
+
+APP_OPTIM := release
+APP_ARM_MODE := arm
+
+APP_CFLAGS := -O2
+APP_CPPFLAGS := -O2
+
+APP_STL := c++_static
+
+
