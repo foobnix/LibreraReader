ead707f0bde12328bb5e042393f20f9eb15aad2d
foobnix <ivan.ivanenko@gmail.com>
2023-04-05 11:28:33 +0300

    update fonts

--- Builder/fonts.sh
+++ Builder/fonts.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-rm ../app/src/fdroid/assets/fonts.zip
-rm ../app/src/main/assets/fonts.zip
-
-zip -r ../app/src/fdroid/assets/fonts.zip fonts
-zip -r fonts/fonts.zip fonts
-#zip -r ../app/src/main/assets/fonts.zip   fonts/FreeSerif.ttf
+cd fonts/ttf
+zip -FSr ../../../app/src/fdroid/assets/fonts.zip .
+zip -FSr ../fonts.zip .
 
