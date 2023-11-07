6e851b98cb3e549add7c902af7914841e8f7cc94
Ivan Ivanenko <ivan.ivanenko@gmail.com>
2023-11-07 11:39:06 +0200

    mupdf 1.23.5

--- Builder/jni/~mupdf-1.23.5/css-apply.c
+++ Builder/jni/~mupdf-1.23.5/css-apply.c
@@ -1430,6 +1430,17 @@ fz_apply_css_style(fz_context *ctx, fz_html_font_set *set, fz_css_style *style, 
 		if (!style->font)
 			style->font = fz_load_html_font(ctx, set, "serif", is_bold, is_italic, style->small_caps);
 	}
+
+
+    style->padding[1].value = style->padding[1].value / 2;
+    style->padding[3].value = style->padding[3].value / 2;
+
+    style->margin[1].value = style->margin[1].value / 2;
+    style->margin[3].value =  style->margin[3].value / 2;
+
+
+    if (style->line_height.value < 0) style->line_height.value = 1.2f;
+
 }
 
 #ifdef DEBUG_CSS_SPLAY
