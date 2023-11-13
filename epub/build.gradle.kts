92282622dafc342b3005adc543bdd8956317b12f
Ivan Ivanenko <ivan.ivanenko@gmail.com>
2023-11-13 11:52:41 +0200

    8.9.58

--- epub/build.gradle.kts
+++ epub/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.3"
+        kotlinCompilerExtensionVersion = "1.5.4"
     }
     packaging {
         resources {
