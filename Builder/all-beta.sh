da7438c420e0cd362eb50011bc6a325b7db067e3
Ivan Ivanenko <ivan.ivanenko@gmail.com>
2023-12-05 18:54:44 +0200

    update build

--- Builder/all-beta.sh
+++ Builder/all-beta.sh
@@ -1,32 +1,33 @@
 #!/usr/bin/env bash
 #./fonts.sh
 
-#Builder folder
-./link_to_mupdf_1.23.7.sh
-
 #/usr/libexec/java_home -V
-#export JAVA_HOME=`/usr/libexec/java_home -v 17`
+export JAVA_HOME=`/usr/libexec/java_home -v 17`
+if [ "$1" == "pc" ]; then
 export JAVA_HOME=/home/dev/.local/share/JetBrains/Toolbox/apps/android-studio/jbr
+fi
+####################################
 
-echo "================== "
-java -version
-echo "================== "
-
+./link_to_mupdf_1.23.7.sh fdroid
 cd ../
 
 ./gradlew clean incVersion
-
 ./gradlew assembleFdroidRelease
-./gradlew assembleProRelease
-#./gradlew assembleLibreraRelease
 
-#cd Builder
-#./link_two_mupdf.sh
-#cd ../
-#./gradlew assembleProRelease
+####################################
+
+cd Builder
+./link_to_mupdf_1.23.7.sh
+cd ../
+
+./gradlew assembleProRelease
+
+####################################
 
 ./gradlew copyApks -Pbeta
 ./gradlew -stop
+
+####################################
 
 rm /home/dev/Dropbox/FREE_PDF_APK/testing/*-x86*
 rm /home/dev/Dropbox/FREE_PDF_APK/testing/*-arm.apk
