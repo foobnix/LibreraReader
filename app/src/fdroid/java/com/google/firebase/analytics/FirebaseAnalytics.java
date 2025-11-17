1ca2a80124c4003b1569b04747d7d20109a18719
Ivan Ivanenko <ivan.ivanenko@gmail.com>
2025-11-17 22:56:48 +0200

    add fdroid

---
+++ app/src/fdroid/java/com/google/firebase/analytics/FirebaseAnalytics.java
@@ -0,0 +1,11 @@
+package com.google.firebase.analytics;
+
+public class FirebaseAnalytics {
+
+    public static FirebaseAnalytics getInstance(Object o) {
+        return new FirebaseAnalytics();
+    }
+
+    public void setUserProperty(String appName, String applicationName) {
+    }
+}
