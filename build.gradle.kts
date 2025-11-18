027d0016b0ec5704d45b44381ff60ab38a2a54ef
Ivan Ivanenko <ivan.ivanenko@gmail.com>
2025-11-18 16:35:23 +0200

    updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
-    id("com.android.application") version "8.12.0" apply false
-    id("com.android.library") version "8.12.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
+    id("com.android.application") version "8.13.0" apply false
+    id("com.android.library") version "8.13.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
     id("androidx.room") version "2.7.2" apply false
-    id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
-    id("com.google.gms.google-services") version "4.4.3" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.0" apply false
-    id("org.jetbrains.kotlin.jvm") version "2.2.0" apply false
+    id("com.google.devtools.ksp") version "2.3.0" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21" apply false
+    id("org.jetbrains.kotlin.jvm") version "2.2.21" apply false
 }
 
 project.extra.apply {
