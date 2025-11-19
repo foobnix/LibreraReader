e6447668269a37abd53b32bb0b535632528e7211
Ivan Ivanenko <ivan.ivanenko@gmail.com>
2025-11-19 12:46:29 +0200

    updates

--- Builder/all-sleep.sh
+++ Builder/all-sleep.sh
@@ -1,2 +1,4 @@
-adb shell am set-standby-bucket com.foobnix.pdf.reader restricted
+
 adb shell am force-stop com.foobnix.pdf.reader
+sleep 1
+adb shell am start-activity -S -W -n com.foobnix.pdf.reader/com.foobnix.ui2.MainTabs2
